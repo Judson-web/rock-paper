20c887b9f9fb11a557877608a60cf864b01e17ea
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-11-23 10:38:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function win(userChoice, cpuChoice) {
   userScore++;
   userScore_span.innerHTML = userScore;
   cpuScore_span.innerHTML = cpuScore;
-  result.innerHTML = `<h1 class="text-win">You win!</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`;
+  result.innerHTML = `<h1 class="text-win">ʏᴏᴜ ᴡɪɴ!</h1> <p>ᴄᴏᴍᴘᴜᴛᴇʀ ᴄʜᴏᴏꜱᴇ<strong>${cpuChoice}</strong></p>`;
   modal.style.display = 'block';
 }
 
@@ -30,14 +30,14 @@ function lose(userChoice, cpuChoice){
   cpuScore++;
   userScore_span.innerHTML = userScore;
   cpuScore_span.innerHTML = cpuScore;
-  result.innerHTML = `<h1 class="text-lose">You lost</h1> <p>Computer choose <strong>${cpuChoice}</strong></p>`; 
+  result.innerHTML = `<h1 class="text-lose">ʏᴏᴜ ʟᴏꜱᴛ</h1> <p>ᴄᴏᴍᴘᴜᴛᴇʀ ᴄʜᴏᴏꜱᴇ<strong>${cpuChoice}</strong></p>`; 
   modal.style.display = 'block'
 }
 
 function draw(userChoice, cpuChoice){
   userScore_span.innerHTML = userScore;
   cpuScore_span.innerHTML = cpuScore;
-  result.innerHTML = `<h1>It's a draw</h1> <p>You both choose <strong>${cpuChoice}</strong></p>`;
+  result.innerHTML = `<h1>ɪᴛ'ꜱ ᴀ ᴅʀᴀᴡ</h1> <p>ʏᴏᴜ ʙᴏᴛʜ ᴄʜᴏᴏꜱᴇ<strong>${cpuChoice}</strong></p>`;
   modal.style.display = 'block'
 }
 
